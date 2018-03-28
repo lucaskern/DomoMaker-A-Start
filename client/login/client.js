1e70d0ba28e4cc0b9c621eb7917e07c11881784b
Lucas Russell Kern <lucaskern8@gmail.com>
2018-03-27 23:52:18 -0400

    finish part D

--- client/login/client.js
+++ client/login/client.js
@@ -48,7 +48,7 @@ const LoginWindow = (props) => {
         <input id="user" type="text" name="username" placeholder="username"/>
         <label htmlFor="pass"> Password: </label>
         <input id="pass" type="password" name="pass" placeholder="password"/>
-        <input type="hidden" name="_csrf" value={props.csrf}/>
+        <input type="hidden" name="_csrf" value={props.csrf} />
         <input className="formSubmit" type="submit" value="Sign in"/>
             
         </form>
@@ -70,8 +70,8 @@ const SignupWindow = (props) => {
         <input id="pass" type="password" name="pass" placeholder="password"/>
         <label htmlFor="pass2"> Password: </label>
         <input id="pass2" type="password" name="pass2" placeholder="retype password"/>
-        <input type="hidden" name="_csrf" value={props.csrf}/>
-        <input className = "formSubmit" type="submit" value="Sign up"/>
+        <input type="hidden" name="_csrf" value={props.csrf} />
+        <input className="formSubmit" type="submit" value="Sign up"/>
         
         </form>
             
